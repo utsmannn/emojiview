d362204fd2fb0fb83e2ba4bdd0927a8273f55ab9
utsmannn <utsmannn@gmail.com>
2024-07-25 12:03:05 +0700

    add: add custom layout for emoji

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "emoji2picker"
+include(":app")
+include(":emoji2-emojipicker")
